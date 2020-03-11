bef5be02befa7e85bba54969feca8b63391ea21f
Daniel Kersten <dkersten1994@gmail.com>
2020-03-11 10:27:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -56,7 +56,7 @@ def movies_with_director_key(name, movies_collection)
   
   while i < movies_collection.length do
     movie_data = movies_collection[i]
-    result.push(movie_with_director_name(name, movie_data))
+    collection_of_movies.push(movie_with_director_name(name, movie_data))
     i += 1
   end
   collection_of_movies
